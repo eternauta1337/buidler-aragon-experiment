c1979beafb4134a80b05f49fa087604f9032c9f6
Alejandro J. Santander <palebluedot@gmail.com>
2019-11-14 14:40:41 -0300

    Introducing very basic tasks

--- buidler.config.js
+++ buidler.config.js
@@ -1,7 +1,16 @@
 usePlugin('@nomiclabs/buidler-truffle5')
 
+// Fooling around with some Aragon related custom tasks.
+require('./tasks/aragon_new_dao')
+require('./tasks/aragon_new_app')
+
 module.exports = {
-  defaultNetwork: 'buidlerevm',
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545'
+    }
+  },
   solc: {
     version: '0.4.24'
   }
