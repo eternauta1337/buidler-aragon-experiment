1fba9e14a97e281703dd245ea314ef7ec7032c41
Alejandro J. Santander <palebluedot@gmail.com>
2019-11-19 07:48:49 -0300

    TS basic bootstrapping

---
+++ buidler.config.ts
@@ -0,0 +1,26 @@
+import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config';
+
+usePlugin('@nomiclabs/buidler-truffle5');
+
+// Fooling around with some Aragon related custom tasks.
+import './tasks/misc/account';
+import './tasks/misc/accounts';
+import './tasks/aragon/new_dao';
+import './tasks/aragon/new_app';
+import './tasks/aragon/app_call';
+import './tasks/aragon/set_permission';
+import './tasks/aragon/has_permission';
+
+const config: BuidlerConfig = {
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545'
+    }
+  },
+  solc: {
+    version: '0.5.10',
+  }
+};
+
+export default config;
